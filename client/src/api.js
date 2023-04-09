19d1ff4df3d3fbf3d98fa9b2f1deef304b27d553
Vishal27-com <vishalgupta962148@gmail.com>
2023-04-10 00:54:05 +0530

    url updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL='http://localhost:8080'
+const BASE_URL='https://blogmate-vl0a.onrender.com'
 export const createUserApi=(creds)=>{
     return axios.post(`${BASE_URL}/users/`,creds)
 }
